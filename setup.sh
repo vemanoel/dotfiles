8ee8a9c2be4291b7a323a0e9c9d54c48b1ca7133
vemanoel <victoremanoelmoraesamaral@gmail.com>
2026-05-16 11:08:45 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,7 @@ sudo zypper install --no-confirm iosevka-fonts
 
 sudo zypper install --no-confirm sway swaybg mako grim slurp fuzzel libnotify-tools
 
-zypper install --no-confirm docker
-sudo systemctl enable --now docker
+sudo zypper install --no-confirm docker
 sudo usermod -G docker -a $USER
 sudo zypper install --no-confirm gcc android-tools libXcursor-devel libXrandr-devel Mesa-libGL-devel libXi-devel libXinerama-devel libXxf86vm-devel libxkbcommon-devel
 go install fyne.io/tools/cmd/fyne@latest
