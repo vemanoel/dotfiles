a41457548958f527c9889bbdeaa3d0b098775faf
vemanoel <victoremanoelmoraesamaral@gmail.com>
2026-05-16 09:25:59 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,7 @@
-zypper install git
+sudo zypper install --no-confirm git
+sudo zypper install --no-confirm go
+sudo zypper install --no-confirm firefox
+sudo zypper install --no-confirm gh
+sudo zypper install --no-confirm glab
+sudo zypper install --no-confirm telegram-desktop
+sudo zypper install --no-confirm zed-editor
