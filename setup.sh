bc38a9b0dc9ee942ce87467089d080728fb5feba
vemanoel <victoremanoelmoraesamaral@gmail.com>
2026-05-16 12:57:40 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env sh
 
+sudo zypper install --no-confirm sway swaylock swayidle swaybg mako grim slurp fuzzel libnotify-tools
+
+sudo zypper install --no-confirm greetd tuigreet
+sudo systemctl disable sddm
+sudo systemctl enable greetd
+
 sudo zypper install --no-confirm firefox htop mpv imv bc dragon-drop telegram-desktop wezterm NetworkManager tar zip unzip fdupes
 sudo zypper install --no-confirm git neovim make go gopls gh glab curl
 sudo zypper install --no-confirm iosevka-fonts
-
-sudo zypper install --no-confirm sway swaybg mako grim slurp fuzzel libnotify-tools
 
 sudo zypper install --no-confirm docker
 sudo usermod -G docker -a $USER
 sudo zypper install --no-confirm gcc android-tools libXcursor-devel libXrandr-devel Mesa-libGL-devel libXi-devel libXinerama-devel libXxf86vm-devel libxkbcommon-devel
 go install fyne.io/tools/cmd/fyne@latest
 go install github.com/fyne-io/fyne-cross@latest
+
+sudo reboot
