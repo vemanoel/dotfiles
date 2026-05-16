a63114bfe49b67d2574264015e6a413cd8e493ba
vemanoel <victoremanoelmoraesamaral@gmail.com>
2026-05-16 10:52:49 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,9 @@
-sudo zypper install --no-confirm firefox
-sudo zypper install --no-confirm git
-sudo zypper install --no-confirm neovim
-sudo zypper install --no-confirm go
-sudo zypper install --no-confirm gopls
-sudo zypper install --no-confirm gh
-sudo zypper install --no-confirm glab
-sudo zypper install --no-confirm telegram-desktop
+sudo zypper install --no-confirm firefox htop mpv imv bc telegram-desktop wezterm NetworkManager tar zip unzip fdupes
+sudo zypper install --no-confirm git neovim make go gopls gh glab curl
 sudo zypper install --no-confirm iosevka-fonts
-sudo zypper install --no-confirm htop
-sudo zypper install --no-confirm make
-sudo zypper install --no-confirm wezterm
+
+sudo zypper install --no-confirm gcc android-tools libXcursor-devel libXrandr-devel Mesa-libGL-devel libXi-devel libXinerama-devel libXxf86vm-devel libxkbcommon-devel
+go install fyne.io/tools/cmd/fyne@latest
+go install github.com/fyne-io/fyne-cross@latest
+
+sudo zypper install --no-confirm sway swaybg mako grim slurp fuzzel libnotify-tools
