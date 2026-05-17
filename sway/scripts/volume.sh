1867d399339277cbd5f41e738bd3d76227ea2618
vemanoel <victoremanoelmoraesamaral@gmail.com>
2026-05-17 19:41:06 -0300

    Update volume.sh

--- sway/scripts/volume.sh
+++ sway/scripts/volume.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 case $1 in
     up)
-        wpctl set-volume @DEFAULT_AUDIO_SINK@ 1%+
+        wpctl set-volume --limit 0.25 @DEFAULT_AUDIO_SINK@ 5%+
         ;;
     down)
-        wpctl set-volume @DEFAULT_AUDIO_SINK@ 1%-
+        wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
         ;;
     mute)
         wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
