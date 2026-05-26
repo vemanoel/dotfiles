193a192e0d275830d46806d41c993aae49a00ed5
vcitoremanoel123 <victoremanoelmoraesamaral@gmail.com>
2026-05-26 09:03:24 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,6 +21,7 @@ sudo zypper install --no-confirm --no-recommends gh # github cli
 sudo zypper install --no-confirm --no-recommends glab # gitlab cli
 sudo zypper install --no-confirm --no-recommends grim slurp # take screenshots
 sudo zypper install --no-confirm --no-recommends ddcutil # monitor manager
+sudo zypper install --no-confirm --no-recommends wl-clipboard # copy/paste
 sudo zypper install --no-confirm wireplumber # volume manager
 sudo zypper install --no-confirm --no-recommends fuzzel # applications runner
 sudo zypper install --no-confirm --no-recommends mako libnotify-tools # notifications manager
